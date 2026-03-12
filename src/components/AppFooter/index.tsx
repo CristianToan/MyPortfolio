7ed9fd8e35a24d615c52eec7138086cfc9bdf057
CristianToan <toanpham9412@gmail.com>
2026-03-12 23:30:00 +0700

    fix: footer

--- src/components/AppFooter/index.tsx
+++ src/components/AppFooter/index.tsx
@@ -8,7 +8,7 @@ const AppFooter = () => {
     <section className="pt-4 pb-18.75">
       <div className="container mx-auto px-4 flex flex-col gap-2.5 justify-center items-center">
 				<h2 className="uppercase bg-title-portfolio bg-clip-text text-transparent font-bold text-xl md:text-xxl tracking-[3.5%]">{t("home.footer.title")}</h2>
-				<div className="text-sm md:text-[18px] tracking-[3] leading-12.5">{t('home.footer.description')}</div>
+				<div className="text-sm md:text-[18px] md:tracking-[3] leading-4 md:leading-12.5 text-center">{t('home.footer.description')}</div>
         {
           LIST_SOCIAL.length > 0 && (
             <ul className="pl-0 mb-0 flex gap-x-3 md:gap-x-8">
